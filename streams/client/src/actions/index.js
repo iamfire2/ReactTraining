3c00bf7da561fb8815294b3c2e618d789ce161eb
Miller <i_am_fire_2@yahoo.com>
2019-04-30 13:50:28 -0500

    History changing for programmatic routing

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -25,31 +25,32 @@ export const signOut = () => {
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get("/streams");
 
-  dispatch({ type: FETCH_STREAMS, payload: response.payload });
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 export const fetchStream = id => async dispatch => {
-  const response = await streams.get(`streams/${id}`);
+  const response = await streams.get(`/streams/${id}`);
 
-  dispatch({ type: FETCH_STREAM, payload: response.payload });
+  dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const createStream = formValues => {
-  return async dispatch => {
-    const response = await streams.post("/streams", formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post("/streams", { ...formValues, userId });
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
   };
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`streams/${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
 export const deleteStream = id => async dispatch => {
-  await streams.delete(`streams/${id}`);
+  await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
 };
--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -5,6 +5,7 @@ import {
   DELETE_STREAM,
   EDIT_STREAM
 } from "../actions/types";
+import _ from "lodash";
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -14,6 +15,10 @@ export default (state = {}, action) => {
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     default:
       return state;
   }
